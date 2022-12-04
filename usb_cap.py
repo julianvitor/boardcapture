51b67196231cb343972dd121d3bd27340be94726
julian <julian.vtr@gmail.com>
2022-12-04 08:28:22 -0300

    general cap path formated

--- usb_cap.py
+++ usb_cap.py
@@ -8,11 +8,15 @@ while True:
     current_time = datetime.datetime.now()
 
     webcam = cv2.VideoCapture(0)
-    frame = webcam.read()[1]
+    frame = webcam.read()[0]
     
+    if (webcam.isOpened()== False): 
+        print("Error opening cam")
+
     folder_path = str(os.getcwd()) + "\images"
-    file_name = "%s-%s-%s:%s:%s:%s.png"%(current_time.day, current_time.month, current_time.year, current_time.hour, current_time.minute, current_time.second)
+    file_name = "%s-%s-%s-%s-%s-%s.png"%(current_time.day, current_time.month, current_time.year, current_time.hour, current_time.minute, current_time.second)
     path = os.path.join(folder_path, file_name)
+    print (path)
     # path = os.path.join("C:\Dev\\boardcapture\\fotos", "teste.png")
     
     time.sleep(3)

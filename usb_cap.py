786fbb55395d890d629cab0b728bfc5a93d4fd4b
julian <julian.vtr@gmail.com>
2022-12-04 08:06:11 -0300

    date file name

--- usb_cap.py
+++ usb_cap.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import cv2
 import time
+import datetime
 import os
 
 while True:
+    current_time = datetime.datetime.now()
+
     webcam = cv2.VideoCapture(0)
     frame = webcam.read()[1]
+    
     folder_path = str(os.getcwd()) + "\images"
-    path = os.path.join(folder_path, "teste.png")
+    file_name = "%s-%s-%s:%s:%s:%s.png"%(current_time.day, current_time.month, current_time.year, current_time.hour, current_time.minute, current_time.second)
+    path = os.path.join(folder_path, file_name)
     # path = os.path.join("C:\Dev\\boardcapture\\fotos", "teste.png")
-    time.sleep(1)
+    
+    time.sleep(3)
 
     cv2.imwrite(path, frame)
-    print("capiturado")
+    print("capiturado em ", path)

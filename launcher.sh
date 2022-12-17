a10ff31782d895d690b2ff7602f83359c284840a
Julian Ambrozio Carreiro <julian.vtr@gmail.com>
2022-12-16 23:58:36 -0300

    arquivo de instrução
    arquivo de instrução para setup do sistema e retornando sudo nos comandos do launcher ao invés de sudo su

--- launcher.sh
+++ launcher.sh
@@ -2,12 +2,11 @@
 # launcher.sh
 echo 'wating for network'
 sleep 10
-sudo su
 cd boardcapture
-git config pull.rebase true
-git pull origin main
-raspi-config nonint do_camera 1 #raspi config enable camera
-yes | apt-get update 
-yes | apt-get install unattended-upgrades #security updates
-yes | pip install -r requirements.txt
-python3 start.py
+sudo git config pull.rebase true
+sudo git pull origin main
+sudo raspi-config nonint do_camera 1 #raspi config enable camera
+yes | sudo apt-get update 
+yes | sudo apt-get install unattended-upgrades #security updates
+yes | sudo pip install -r requirements.txt
+sudo python3 start.py

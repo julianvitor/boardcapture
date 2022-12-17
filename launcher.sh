59f7068bae4d20b68db3741ac0ecdaf82eb788cd
Julian Ambrozio Carreiro <julian.vtr@gmail.com>
2022-12-16 22:10:08 -0300

    log para crontab

--- launcher.sh
+++ launcher.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # launcher.sh
 echo 'wating for network'
-sleep 5
-sudo git config pull.rebase true
-sudo git pull origin main
-sudo raspi-config nonint do_camera 1 #raspi config enable camera
-sudo yes | sudo apt-get update 
-sudo yes | sudo apt-get install unattended-upgrades #security updates
-sudo yes | sudo pip install -r requirements.txt
-sudo git config pull.rebase true
-sudo git pull origin main
-sudo python3 start.py
+sleep 10
+sudo su
+cd boardcapture
+git config pull.rebase true
+git pull origin main
+raspi-config nonint do_camera 1 #raspi config enable camera
+yes | apt-get update 
+yes | apt-get install unattended-upgrades #security updates
+yes | pip install -r requirements.txt
+python3 start.py

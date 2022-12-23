9a8224eb1ba26109c056ecb22f09c918336f006c
Julian Ambrozio Carreiro <julian.vtr@gmail.com>
2022-12-23 15:32:00 -0300

    inifinite loop pip headless

--- launcher.sh
+++ launcher.sh
@@ -6,8 +6,11 @@ cd boardcapture
 sudo git config pull.rebase true
 sudo git pull origin main
 sudo raspi-config nonint do_camera 1 #raspi config enable camera
-yes | sudo apt-get update 
-yes | sudo apt-get install unattended-upgrades #security updates
-yes | sudo apt-get install pip
+yes | sudo apt update 
+yes | sudo apt install unattended-upgrades #security updates
+yes | sudo timedatectl set-ntp true
+yes | sudo systemctl restart systemd-timesyncd
+yes | sudo apt install pip
+yes | sudo pip install --upgrade pip setuptools wheel
 yes | sudo pip install -r requirements.txt
 sudo python3 start.py
